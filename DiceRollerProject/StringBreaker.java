94b236cd3366ead4e40f5ac6d0907e936b2f22de
MrTrauger <149713045+MrTrauger@users.noreply.github.com>
2024-01-23 13:51:29 -1000

    Update StringBreaker.java
    Attempted to fix indentation for easier readability.

--- DiceRollerProject/StringBreaker.java
+++ DiceRollerProject/StringBreaker.java
@@ -1,30 +1,30 @@
-          int d = roll.indexOf('d');
-					
-					int plus = roll.indexOf('+');
-					
-					if (d > 0 && plus > 0)
-          {
-						
-  						int count = Integer.valueOf(roll.substring(0, d)) ;
-  						
-  						int faces = Integer.valueOf(roll.substring(d+1, plus)) ;
-  						
-  						total += rollDice(count, faces) ;
-  						
-  						roll = roll.substring(plus+1) ;
-  						
-  						System.out.print(count + " " + faces + "-sided dice, and ") ;
-					}
-					
-					else if (d > 0 && plus == -1) 
-          {
-  						int count = Integer.valueOf(roll.substring(0, d)) ;
-  						
-  						int faces = Integer.valueOf(roll.substring(d+1)) ;
-  						
-  						total += rollDice(count, faces) ; 
-  						
-  						roll = "";
-  						
-  						System.out.println(count + " " + faces + "-sided dice.");	
-					}
+int d = roll.indexOf('d');
+
+int plus = roll.indexOf('+');
+
+if (d > 0 && plus > 0)
+{
+	
+	int count = Integer.valueOf(roll.substring(0, d)) ;
+	
+	int faces = Integer.valueOf(roll.substring(d+1, plus)) ;
+	
+	total += rollDice(count, faces) ;
+	
+	roll = roll.substring(plus+1) ;
+	
+	System.out.print(count + " " + faces + "-sided dice, and ") ;
+}
+
+else if (d > 0 && plus == -1) 
+{
+	int count = Integer.valueOf(roll.substring(0, d)) ;
+	
+	int faces = Integer.valueOf(roll.substring(d+1)) ;
+	
+	total += rollDice(count, faces) ; 
+	
+	roll = "";
+	
+	System.out.println(count + " " + faces + "-sided dice.");	
+}
