9c26a33b979e5f4c7dddd289ae1723ba620790c0
MrTrauger <149713045+MrTrauger@users.noreply.github.com>
2024-08-14 07:58:39 -1000

    Create IntroSyntaxAndSystem.java

---
+++ DebugPractice/IntroSyntaxAndSystem.java
@@ -0,0 +1,22 @@
+/*
+  For each task below, explain why the code is incorrect, what type of error that is, and how to fix it.
+*/
+
+//Task 1: Print out "Hello World"
+System.out.print("Hello World);
+
+//Task 2: Print out "Hello World"
+System.out.print(Hello World);
+
+//Task 3: Print out "Hello World"
+System.Out.Print("Hello World");
+
+//Task 4: Print out "Hello World"
+System.out.print("Hello World")
+
+//Task 5: Print out "Hello World"
+System.out.print("Hello Wrold")
+
+//Task 6: Print out "Hello World twice, one on top of the other"
+System.out.print("Hello World");
+System.out.print("Hello World");
